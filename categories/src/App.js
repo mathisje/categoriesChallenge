e42c5ab4beade1274d35db5c2d0d7c60e1f04fb4
Jeff <mathisjeffr@gmail.com>
2018-04-12 01:17:45 -0700

    initial commit

---
+++ categories/src/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesById: {
+        100: {
+          parentCategoryId: '-1',
+          name: 'Business',
+          keywords: 'Money',
+          level: '1'
+        },
+        200: {
+          parentCategoryId: '-1',
+          name: 'Tutoring',
+          keywords: 'Teaching',
+          level: '1'
+        },
+        101: {
+          parentCategoryId: '100',
+          name: 'Accounting',
+          keywords: 'Taxes',
+          level: '2'
+        },
+        102: {
+          parentCategoryId: '100',
+          name: 'Taxation',
+          keywords: '',
+          level: '2'
+        },
+        201: {
+          parentCategoryId: '200',
+          name: 'Computer',
+          keywords: '',
+          level: '2'
+        },
+        103: {
+          parentCategoryId: '101',
+          name: 'Corporate Tax',
+          keywords: '',
+          level: '3'
+        },
+        202: {
+          parentCategoryId: '201',
+          name: 'Operating System',
+          keywords: '',
+          level: '3'
+        },
+        109: {
+          parentCategoryId: '101',
+          name: 'Small business Tax',
+          keywords: '',
+          level: '3'
+        }
+      },
+      categoriesByLevel: {
+        1: ['100','200'],
+        2: ['101','102','201'],
+        3: ['103','109','202']
+      },
+      searchParam: '',
+      newCategoryId: '',
+      newParentId: '',
+      newName: '',
+      newKeywords: ''
+    };
+  }
+
+  addCategory = (state, category) => {
+
+  };
+
+
+  findCategories = (state, id) => {
+
+  };
+
+  clearCategories = () => {
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div>
+          <button>Clear Categories</button>
+        </div>
+        <div>
+          <div>
+            id <input />
+          </div>
+          <div>
+            parent id <input />
+          </div>
+          <div>
+            name <input />
+          </div>
+          <div>
+            keywords <input />
+          </div>
+          <div>
+            <button>Add Category</button>
+          </div></div>
+        <div>
+          <input />
+          <button>Search</button>
+        </div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default App;
